697fa0a1e3b2a354beb5124ecd58b96fbe2ba5e2
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-11 12:23:06 -0400

    added the promptAddRole

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ const selectRoles = () => {
 };
 const selectEmployees = () => {
     connection.query(
-        'SELECT * FROM employee;',
+        "SELECT E.id, E.first_name, E.last_name, R.title, D.name AS department, R.salary, CONCAT(M.first_name,' ',M.last_name) AS manager FROM employee E JOIN role R ON E.role_id = R.id JOIN department D ON R.department_id = D.id JOIN employee M ON E.manager_id = M.id;",
         (err, results) => {
             console.table(results); // results contains rows returned by server
             promptMenu();
@@ -78,64 +78,97 @@ const selectEmployees = () => {
     )
 };
 
-// return inquirer.prompt([
-//     {
-//         type: 'input',
-//         name: 'name',
-//         message: 'What is your name? (Required)',
-//         validate: nameInput => {
-//             if (nameInput) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter your name!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'employeeId',
-//         message: 'Enter your employee ID (Required)',
-//         validate: employeeId => {
-//             if (employeeId) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter your employee ID!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'email',
-//         message: 'Enter your email address (Required)',
-//         validate: email => {
-//             if (email) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter your email address!');
-//                 return false;
-//             }
-//         }
-//     },
-//     {
-//         type: 'input',
-//         name: 'officeNumber',
-//         message: 'Enter your office number (Required)',
-//         validate: officeNumber => {
-//             if (officeNumber) {
-//                 return true;
-//             } else {
-//                 console.log('Please enter your office number!');
-//                 return false;
-//             }
-//         }
-//     },
-// ]).then(answers => {
-//     console.log(answers);
-//     const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
-//     teamMembers.push(manager);
-//     promptMenu();
-// })
+const promptAddDepartment = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the department? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your department!');
+                    return false;
+                }
+            }
+        },
+    ]).then(answers => {
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+        promptMenu();
+    })
+}
+
+const promptAddRole = () => {
+    return connection.promise().query(
+        'SELECT * FROM department;',
+    ).then((res) => {
+        let departments = [];
+        for (let i = 0; i > result[0].length; i++) {
+            // push values into the array
+            departments.push()
+        }
+        console.log(res[0].TextRow.name)
+        // make it a drop down
+        return {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: departments
+        }
+    }
+    ).catch((err) =>
+        console.log(err)
+    )
+
+    // return inquirer.prompt([
+    //     {
+    //         type: 'input',
+    //         name: 'name',
+    //         message: 'Enter the name of your title (Required)',
+    //         validate: titleName => {
+    //             if (titleName) {
+    //                 return true;
+    //             } else {
+    //                 console.log('Please enter your title name!');
+    //                 return false;
+    //             }
+    //         }
+    //     },
+    //     {
+    //         type: 'list',
+    //         name: 'department',
+    //         message: 'Which department are you from?',
+    //         choices: departmentChoices
+    //     },
+    //     {
+    //         type: 'input',
+    //         name: 'salary',
+    //         message: 'Enter your salary (Required)',
+    //         validate: salary => {
+    //             if (salary) {
+    //                 return true;
+    //             } else {
+    //                 console.log('Please enter your salary!');
+    //                 return false;
+    //             }
+    //         }
+    //     },
+    //     {
+    //         type: 'input',
+    //         name: 'department',
+    //         message: 'Enter your department name (Required)',
+    //         validate: department => {
+    //             if (department) {
+    //                 return true;
+    //             } else {
+    //                 console.log('Please enter your department name!');
+    //                 return false;
+    //             }
+    //         }
+    //     }
+}
 
 promptMenu();
